4c0ead1d91a1e91ba97300bf3372c27f93be3d6c
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-09-15 17:09:09 +0200

    pokedex entry ui

--- app/src/main/java/com/example/pokedex_tutorial/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedex_tutorial/pokemonlist/PokemonListScreen.kt
@@ -2,31 +2,49 @@ package com.example.pokedex_tutorial.pokemonlist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.*
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.request.ImageRequest
 import com.example.pokedex_tutorial.R
+import com.example.pokedex_tutorial.data.models.PokedexListEntry
+import com.example.pokedex_tutorial.ui.theme.RobotoCondensed
+import com.google.accompanist.coil.CoilImage
 
 @Composable
 fun PokemonListScreen(
@@ -47,7 +65,9 @@ fun PokemonListScreen(
             )
             SearchBar(
                 hint = "Search...",
-                modifier = Modifier.fillMaxWidth().padding(16.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
             ) {
 
             }
@@ -95,4 +115,64 @@ fun SearchBar(
             )
         }
     }
+}
+
+
+@Composable
+fun PokedexEntry(
+    entry: PokedexListEntry,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colorScheme.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+
+    Box(
+        contentAlignment = Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(listOf(dominantColor, defaultDominantColor))
+            )
+            .clickable {
+                navController.navigate(
+                    "pokemon_detail_screen/${dominantColor.toArgb()}/${entry.pokemonName}"
+                )
+            }
+    ) {
+        CoilImage(
+            request = ImageRequest.Builder(LocalContext.current)
+                .data(entry.imageUrl)
+                .target{
+                    viewModel.calcDominantColor(it) { color ->
+                        dominantColor = color
+                    }
+                }
+                .build(),
+            contentDescription = entry.pokemonName,
+            fadeIn = true,
+            modifier = Modifier
+                .size(120.dp)
+                .align(Center)
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.scale(0.5f)
+            )
+        }
+        Text(
+            text = entry.pokemonName,
+            fontFamily = RobotoCondensed,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+
+
 }
