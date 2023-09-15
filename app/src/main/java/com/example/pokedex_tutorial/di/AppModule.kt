64c25c02da6efed693e1f4a80845cbfd9d9af2fe
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-09-15 14:50:24 +0200

    dagger setup

---
+++ app/src/main/java/com/example/pokedex_tutorial/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.pokedex_tutorial.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
