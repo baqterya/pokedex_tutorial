9293b086651805e9d6e3ac14caa0933435dfba77
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-09-21 13:11:35 +0200

    updated all dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-//    id("com.google.devtools.ksp")
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
@@ -81,7 +80,7 @@ dependencies {
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
 
     // Timber
@@ -96,19 +95,18 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     // Coil
-    implementation("io.coil-kt:coil:1.1.1")
-    implementation("com.google.accompanist:accompanist-coil:0.7.0")
+    implementation("io.coil-kt:coil:2.4.0")
+    implementation("com.google.accompanist:accompanist-coil:0.15.0")
 
     //Dagger - Hilt
-    implementation("com.google.dagger:dagger:2.44.2")
-    kapt("com.google.dagger:dagger-compiler:2.44.2")
+    implementation("com.google.dagger:dagger:2.48")
+    kapt("com.google.dagger:dagger-compiler:2.48")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
-    implementation("com.google.dagger:hilt-android:2.44.2")
-//    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("com.google.dagger:hilt-android:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
     kapt("androidx.hilt:hilt-compiler:1.1.0-alpha01")
-    kapt("com.google.dagger:hilt-android-compiler:2.44.2")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 
 
     implementation("androidx.palette:palette-ktx:1.0.0")
